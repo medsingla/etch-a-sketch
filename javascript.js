fe45b46073015da1587bd6c4a9240b94380c084c
medsingla <medsingla@gmail.com>
2023-07-27 13:30:14 -0400

    Add initial tile functionality
    Make tile turn green when hovering and black on click

--- javascript.js
+++ javascript.js
@@ -4,3 +4,28 @@ for(let i = 0; i < 16; i++) {
     let div = document.createElement('div');
     container.appendChild(div);
 }
+
+let tiles = document.querySelectorAll('#container > div');
+
+tiles.forEach(changeCell);
+tiles.forEach(draw);
+
+function changeCell(div) {
+    if(true) {
+        div.addEventListener("mouseover", function() {
+            div.style.cssText = "background: green;";
+        });
+        div.addEventListener("mouseout", function remove() {
+            div.style.cssText = "background: transparent;";
+        });
+    }
+}
+
+function draw(div) {
+    if(true) {
+        div.removeEventListener("mousedown", remove);
+        div.addEventListener("mousedown", function() {
+            div.style.cssText = "background: black;";
+        });
+    }
+}
