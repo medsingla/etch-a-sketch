73a991a63df9060cab678fadfde5b88d5fdb7f3d
medsingla <medsingla@gmail.com>
2023-07-27 15:25:47 -0400

    Finish hovering + drawing javascript code
    Tiles turn green when mouse hovers over, turn black when click/click drag.

--- javascript.js
+++ javascript.js
@@ -1,31 +1,51 @@
+let mode = false;
+
 const container = document.querySelector('#container');
 
 for(let i = 0; i < 16; i++) {
     let div = document.createElement('div');
+    div.style.background = "transparent";
+    div.addEventListener("mousedown", drawMode);
+    div.addEventListener("mouseover", makeColor);
+    div.addEventListener("mouseout", makeTrans);
+    div.addEventListener("mouseup", hoverMode);
     container.appendChild(div);
 }
 
 let tiles = document.querySelectorAll('#container > div');
 
-tiles.forEach(changeCell);
-tiles.forEach(draw);
-
-function changeCell(div) {
-    if(true) {
-        div.addEventListener("mouseover", function() {
-            div.style.cssText = "background: green;";
-        });
-        div.addEventListener("mouseout", function remove() {
-            div.style.cssText = "background: transparent;";
-        });
+function makeColor(e) {
+    if(!mode) {
+        if (e.target.style.background == "transparent") {
+            e.target.style.cssText = "background: green;";
+        }
+    }
+    else {
+        e.target.style.cssText = "background: black;";
     }
 }
 
-function draw(div) {
-    if(true) {
-        div.removeEventListener("mousedown", remove);
-        div.addEventListener("mousedown", function() {
-            div.style.cssText = "background: black;";
-        });
+function makeTrans(e) {
+    e.target.style.cssText = "background: transparent";
+}
+
+function drawMode(e) {
+    mode = true;
+    e.target.style.cssText = "background: black";
+    tiles.forEach(changeToDraw);
+}
+
+function changeToDraw(div) {
+    div.removeEventListener("mouseout", makeTrans);
+}
+
+function changeToHover(div) {
+    if (div.style.background == "transparent") {
+        div.addEventListener("mouseout", makeTrans);
     }
+}
+
+function hoverMode(div) {
+    mode = false;
+    tiles.forEach(changeToHover);
 }
